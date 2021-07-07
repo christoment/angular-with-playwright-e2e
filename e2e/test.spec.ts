be28af27d617586d6a70108eb1296031560572f9
Chris <christoment@gmail.com>
2021-07-07 17:34:47 +1000

    Update readme + fix port

--- e2e/test.spec.ts
+++ e2e/test.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
-  await page.goto('http://localhost:4343');
+  await page.goto('http://localhost:4200');
   const url = await page.url();
   expect(url).toMatch(new RegExp(`.*/home`));
   expect(await page.textContent('text=This is page #0')).toBeTruthy();
